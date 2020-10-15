05a67af315e7b60beedf9f056b5a14a436371d33
Lucas Celen <r0710760@student.thomasmore.be>
2020-10-15 15:54:54 +0200

    basic blade & automation

--- public/js/app.js
+++ public/js/app.js
@@ -37346,4 +37346,3 @@ module.exports = __webpack_require__(/*! C:\sites_laravel\student_administration
 /***/ })
 
 /******/ });
-//# sourceMappingURL=app.js.map
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,9 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps(true, 'source-map')
-    .version();
+    .sass('resources/sass/app.scss', 'public/css');
 
 mix.browserSync({
     proxy: 'student_administration.test',
